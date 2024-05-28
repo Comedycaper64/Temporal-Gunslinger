c91502607a07d78ee5e431e56310404a91325700
Matvey Shchetinin <mshchetinin64@gmail.com>
2024-05-28 21:35:38 +0100

    Improved level retry UI, improves sfx, adds animation offset randomisation

---
+++ Assets/Scripts/Managers/AnimatorSpeedRandomiser.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSpeedRandomiser : MonoBehaviour
+{
+    [SerializeField]
+    private float minRandomMult = 0f;
+
+    [SerializeField]
+    private float maxRandomMult = 1f;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("random", Random.Range(minRandomMult, maxRandomMult));
+    }
+}
--- Assets/Scripts/UI/LevelRetryUI.cs
+++ Assets/Scripts/UI/LevelRetryUI.cs
@@ -5,13 +5,52 @@ using UnityEngine;
 
 public class LevelRetryUI : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject retryUI;
+    private float dampVelocity1;
+    private float dampVelocity2;
+    private CanvasGroup canvasGroup;
+    private float targetAlpha;
+    private bool changeOpacity = false;
+    private float uiScale = 1.4f;
+    private float targetScale = 1f;
 
     private void Start()
     {
         GameManager.Instance.OnLevelLost += GameManager_OnLevelLost;
-        retryUI.SetActive(false);
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0f;
+        transform.localScale = new Vector3(uiScale, uiScale, uiScale);
+    }
+
+    private void Update()
+    {
+        if (changeOpacity)
+        {
+            canvasGroup.alpha = Mathf.SmoothDamp(
+                canvasGroup.alpha,
+                targetAlpha,
+                ref dampVelocity1,
+                0.5f,
+                Mathf.Infinity,
+                Time.unscaledDeltaTime
+            );
+
+            float newScale = Mathf.SmoothDamp(
+                transform.localScale.x,
+                targetScale,
+                ref dampVelocity2,
+                0.5f,
+                Mathf.Infinity,
+                Time.unscaledDeltaTime
+            );
+
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            if (Mathf.Abs(canvasGroup.alpha - targetAlpha) < 0.01f)
+            {
+                canvasGroup.alpha = targetAlpha;
+                changeOpacity = false;
+            }
+        }
     }
 
     private void OnDisable()
@@ -21,6 +60,16 @@ public class LevelRetryUI : MonoBehaviour
 
     private void GameManager_OnLevelLost(object sender, bool e)
     {
-        retryUI.SetActive(e);
+        changeOpacity = true;
+        if (e)
+        {
+            targetAlpha = 1f;
+            targetScale = 1f;
+        }
+        else
+        {
+            targetAlpha = 0f;
+            targetScale = uiScale;
+        }
     }
 }
